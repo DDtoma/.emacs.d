2cafc6adeda0c41275bb9921a80917edb0a29b30
root <root@light.me>
2019-07-29 11:15:55 +0800

    change config struct

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -26,27 +26,6 @@ If the new path's directories does not exist, create them."
 	  exec-path-from-shell-arguments '("-l" "-i"))
     (exec-path-from-shell-initialize)))
 
-;; Key Modifiers
-(with-no-warnings
-  (cond
-   (sys/win32p
-    ;; make PC keyboard's Win key or other to type Super or Hyper
-    ;; (setq w32-pass-lwindow-to-system nil)
-    (setq w32-lwindow-modifier 'super     ; Left Windows key
-	  w32-apps-modifier 'hyper)       ; Menu/App key
-    (w32-register-hot-key [s-t]))
-   ((and sys/macp (eq window-system 'mac))
-    ;; Compatible with Emacs Mac port
-    (setq mac-option-modifier 'super
-	  mac-command-modifier 'meta)
-    (bind-keys ([(super a)] . mark-whole-buffer)
-	       ([(super c)] . kill-ring-save)
-	       ([(super l)] . goto-line)
-	       ([(super q)] . save-buffers-kill-emacs)
-	       ([(super s)] . save-buffer)
-	       ([(super v)] . yank)
-	       ([(super w)] . delete-frame)
-	       ([(super z)] . undo)))))
 
 (use-package recentf
   :ensure nil
@@ -84,17 +63,17 @@ If the new path's directories does not exist, create them."
   (setq dired-dwim-target t)
 
   ;; enable some really cool extensions like C-x C-j(dired-jump)
-  (require 'dired-x))
-
-(use-package dired-single
-  :ensure t
-  :init
-  (defun my-dired-init ()
-    (define-key dired-mode-map [return] 'dired-single-buffer)
-    (define-key dired-mode-map [mouse-1] 'dired-single-buffer-mouse)
-    (define-key dired-mode-map "^" 'dired-single-up-directory))
-  :hook
-  (dired-mode . my-dired-init)
+  (require 'dired-x)
+  (use-package dired-single
+    :ensure t
+    :init
+    (defun my-dired-init ()
+      (define-key dired-mode-map [return] 'dired-single-buffer)
+      (define-key dired-mode-map [mouse-1] 'dired-single-buffer-mouse)
+      (define-key dired-mode-map "^" 'dired-single-up-directory))
+    :hook
+    (dired-mode . my-dired-init)
+    )
   )
 
 
@@ -107,12 +86,12 @@ If the new path's directories does not exist, create them."
   :bind
   (("C-x c" . avy-goto-char)
    ("C-x l" . avy-goto-line))
+  :config
+  (use-package all-the-icons-ivy
+    :ensure t
+    :init
+    (ivy-set-display-transformer 'ivy-switch-buffer 'all-the-icons-ivy--buffer-transformer))
   )
-
-(use-package all-the-icons-ivy
-  :ensure t
-  :init
-  (ivy-set-display-transformer 'ivy-switch-buffer 'all-the-icons-ivy--buffer-transformer))
 
 (use-package swiper
   :bind
--- lisp/init-keymap.el
+++ lisp/init-keymap.el
@@ -1,3 +1,25 @@
+;; Key Modifiers
+(with-no-warnings
+  (cond
+   (sys/win32p
+    ;; make PC keyboard's Win key or other to type Super or Hyper
+    ;; (setq w32-pass-lwindow-to-system nil)
+    (setq w32-lwindow-modifier 'super     ; Left Windows key
+	  w32-apps-modifier 'hyper)       ; Menu/App key
+    (w32-register-hot-key [s-t]))
+   ((and sys/macp (eq window-system 'mac))
+    ;; Compatible with Emacs Mac port
+    (setq mac-option-modifier 'super
+	  mac-command-modifier 'meta)
+    (bind-keys ([(super a)] . mark-whole-buffer)
+	       ([(super c)] . kill-ring-save)
+	       ([(super l)] . goto-line)
+	       ([(super q)] . save-buffers-kill-emacs)
+	       ([(super s)] . save-buffer)
+	       ([(super v)] . yank)
+	       ([(super w)] . delete-frame)
+	       ([(super z)] . undo)))))
+
 ;; S-shift s-super m-meta <delete>
 (global-set-key (kbd "s-\\") 'llight//delete-blank-lines)
 (global-set-key (kbd "C-c k") 'kill-this-buffer)
