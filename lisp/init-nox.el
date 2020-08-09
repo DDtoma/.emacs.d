3a2664ab0f982bf265f29ab010f4f6d86d8b74fb
llight <ll66456645@gmail.com>
2020-08-09 13:17:20 +0800

    remove nox config

--- lisp/init-nox.el
+++ lisp/init-nox.el
@@ -1,16 +1,16 @@
 (use-package posframe
   :ensure t)
 
-(use-package nox
-  :load-path "~/.emacs.d/plugin/nox"
-  :ensure nil
-  :config
-  (setq nox-python-server 'pyls)
-  :hook
-  (emacs-lisp-mode . (lambda () (require 'nox) (nox-ensure)))
-  (lisp-mode . (lambda () (require 'nox) (nox-ensure)))
-  (python-mode . (lambda () (require 'nox) (nox-ensure)))
-  (java-mode . (lambda () (require 'nox) (nox-ensure)))
-  )
+;; (use-package nox
+;;   :load-path "~/.emacs.d/plugin/nox"
+;;   :ensure nil
+;;   :config
+;;   (setq nox-python-server 'pyls)
+;;   :hook
+;;   (emacs-lisp-mode . (lambda () (require 'nox) (nox-ensure)))
+;;   (lisp-mode . (lambda () (require 'nox) (nox-ensure)))
+;;   (python-mode . (lambda () (require 'nox) (nox-ensure)))
+;;   (java-mode . (lambda () (require 'nox) (nox-ensure)))
+;;   )
 
 (provide 'init-nox)
