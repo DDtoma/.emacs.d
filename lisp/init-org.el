da5e41da29b7ab64b707524b79d2664386e43fc0
llight <ll66456645@163.com>
2024-07-24 09:44:48 +0800

    配置orgmode priority

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,6 +1,7 @@
 (use-package org
   :ensure nil
   :defer nil
+  :straight (:type built-in)
   :hook
   ((org-mode . (lambda ()
                  "Beautify Org Checkbox Symbol"
@@ -21,6 +22,9 @@
                         (setq show-paren-mode nil)))
    )
   :config
+  ;; 关闭转译字符下划线 _和^  Non-nil means interpret \"_\" and \"^\" for display.
+  (setq-default org-use-sub-superscripts nil)
+
   (defhydra hydra-global-org (:color blue :hint nil)
     "
 Timer^^        ^Clock^         ^Capture^
@@ -45,36 +49,36 @@ _p_rint
 
   (global-set-key [f11] 'hydra-global-org/body)
 
-  (setq org-agenda-files '("~/Documents/Note" "~/Documents/org-roam/daily")
+  (setq org-agenda-files '("~/Documents/org-roam/")
         org-todo-keywords '((sequence "TODO(t)" "DOING(i)" "HANGUP(h)" "|" "DONE(d)" "CANCEL(c)" "DELAY(D)"))
         org-todo-keyword-faces '(("HANGUP" . warning)
                                  ("❓" . warning))
         org-log-done 'time
         org-catch-invisible-edits 'smart
         org-startup-indented t
-        org-ellipsis (if (char-displayable-p ?) "  " nil)
+        org-ellipsis (if (char-displayable-p ?) " " nil)
         org-pretty-entities t
-        org-hide-emphasis-markers t)
+        org-hide-emphasis-markers t
 
-  (setq org-capture-templates
-        '(("t" "Todo" entry (file+headline "~/Documents/Note/gtd.org" "Tasks")
-           "* TODO %?\n %i\n %a")
-          ("j" "Journal" entry (file+datetree "~/Documents/Note/journal.org")
-           "* %?\nEntered on %U\n %i\n %a")
-          ("i" "Checkitem" checkitem (file+headline "~/Documents/Note/gtd.org" "ItemBox")
-           "[ ] %?\n - [ ] Enter Item\n\n %U\n")))
-  ;; (setq org-agenda-time-grid (quote ((daily today require-timed)
-  ;;                                    (300
-  ;;                                     600
-  ;;                                     900
-  ;;                                     1200
-  ;;                                     1500
-  ;;                                     1800
-  ;;                                     2100
-  ;;                                     2400)
-  ;;                                    "......"
-  ;;                                    "----------------------------------------------------"
-  ;;                                    )))
+        ;; 优先级范围和默认任务的优先级
+        org-highest-priority ?A
+        org-lowest-priority  ?D
+        org-default-priority ?D
+        org-priority-faces '(
+                             (?A . (:background "yellow" :foreground "red" :weight bold))
+                             (?B . (:background "DarkOrange" :foreground "white" :weight bold))
+                             (?C . (:background "yellow" :foreground "DarkGreen" :weight bold))
+                             (?D . (:background "DodgerBlue" :foreground "black" :weight bold))
+                             )
+        )
+
+  ;; (setq org-capture-templates
+  ;;       '(("t" "Todo" entry (file+headline "~/Documents/Note/gtd.org" "Tasks")
+  ;;          "* TODO %?\n %i\n %a")
+  ;;         ("j" "Journal" entry (file+datetree "~/Documents/Note/journal.org")
+  ;;          "* %?\nEntered on %U\n %i\n %a")
+  ;;         ("i" "Checkitem" checkitem (file+headline "~/Documents/Note/gtd.org" "ItemBox")
+  ;;          "[ ] %?\n - [ ] Enter Item\n\n %U\n")))
   )
 
 (use-package org-modern
@@ -101,14 +105,7 @@ _p_rint
    org-agenda-block-separator ?─
    org-agenda-time-grid
    '((daily today require-timed)
-     (300
-      600
-      900
-      1200
-      1500
-      1800
-      2100
-      2400)
+     (300 600 900 1200 1500 1800 2100 2400)
      " ┄┄┄┄┄ "
      "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
    org-agenda-current-time-string
